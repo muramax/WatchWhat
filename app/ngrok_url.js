f4d1d485aec32ec98df31dc6b9123149c89a3068
Sushi <kaopansri@gmail.com>
2025-05-12 14:48:23 +0700

    publish version

--- app/ngrok_url.js
+++ app/ngrok_url.js
@@ -1,1 +1,1 @@
-export const NGROK_URL = "https://4bcc-34-68-19-216.ngrok-free.app/";
+export const NGROK_URL = "";
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -8,13 +8,13 @@ import { getFirestore, collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBTegJ0nbuaCqAx9_FAGkc4DH_N5D3Q738",
-  authDomain: "watchwhat-2190512.firebaseapp.com",
-  projectId: "watchwhat-2190512",
-  storageBucket: "watchwhat-2190512.firebasestorage.app",
-  messagingSenderId: "281641424310",
-  appId: "1:281641424310:web:e295575de09b6f33e85d17",
-  measurementId: "G-DQ6FNVYG3Y",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
